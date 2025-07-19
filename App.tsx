6df9a083bea84f6aa880e155c03f2deef2db1ae3
Ekrem <70305889+yakupozcan@users.noreply.github.com>
2025-07-19 13:57:26 +0300

    flex uygulandı

--- App.tsx
+++ App.tsx
@@ -6,7 +6,9 @@
  */
 
 // aimport { NewAppScreen } from '@react-native/new-app-screen';
-import { /*StatusBar,*/ StyleSheet, /*useColorScheme,*/ View, Text, Button} from 'react-native';
+import { StatusBar, StyleSheet,SafeAreaView, /*useColorScheme,*/ View, Text, Button} from 'react-native';
+//import { StatusBar } from 'react-native/types_generated/index';
+//import { SafeAreaView } from 'react-native/types_generated/index';
 //import { Button } from 'react-native/types_generated/index';
 
 function App() {
@@ -14,17 +16,31 @@ function App() {
     console.log("Merhaba React")
   }
   return (
-    <View>
-      <Text>Merhaba Yakup</Text>
-      <Button title="Press me" onPress={() => {sayHello()}} disabled={false}/>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{justifyContent: 'center',alignContent:'center' ,alignItems: 'center',backgroundColor: 'orange', height: StatusBar.currentHeight}}> 
+        <Text>Notifications</Text> 
+        </View>
+      <View style={[styles.upper_view_container,{flex:1}]}>
+            <Text>MerhabaYakup</Text>
+           <Button title="Press me" onPress={() => {sayHello()}} /> 
     </View>
-  );
-}
+    <View style={[styles.bottom_view_container,{flex:1}]}>
+      <Text> hoşgeldiniz </Text>
+    </View>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+    </SafeAreaView>
+    
+  );
+}        
+
+ const styles = StyleSheet.create({
+  upper_view_container: {
+    backgroundColor: 'red'
   },
+  bottom_view_container: {
+    backgroundColor: 'blue',
+    alignItems: 'center'
+  }
 });
 
 export default App;
